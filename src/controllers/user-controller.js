d4679f4e1756c9ee30aa86e2fd44b7040a2726dc
vishalthakur <vishalthakur970820@gmail.com>
2025-07-28 14:06:50 +0530

    Added new user signup api

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,26 @@
+const { StatusCodes } = require('http-status-codes')
+const { SuccessResponse, ErrorResponse } = require('../utils/common')
+const { UserService } = require('../services')
+
+async function signup(req, res) {
+    try {
+        const user = await UserService.create({
+            email: req.body.email,
+            password: req.body.password
+        })
+        SuccessResponse.data = user
+        return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse)
+    }
+    catch (error) {
+        ErrorResponse.error = error
+        return res
+            .status(error.statusCode)
+            .json(ErrorResponse)
+    }
+}
+
+module.exports = {
+    signup
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const userRoutes = require('./user-routes')
+
+router.use('/signup', userRoutes)
+
 module.exports= router
